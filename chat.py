bd77a44e164e421235b9352bf06d69d322d0a74b
Zekun Zhang <kzz1994@gmail.com>
2024-04-23 00:43:59 -0400

    real-time generation

--- chat.py
+++ chat.py
@@ -13,6 +13,7 @@ import fire
 
 import torch
 from llama import Dialog, Llama
+from llama.generation import sample_top_p
 
 
 def read_text_file(filename):
@@ -59,13 +60,69 @@ def fetch_url(url):
     return text
 
 
+@torch.inference_mode()
+def generate_print(
+    generator,
+    prompt_tokens: List[int],
+    max_gen_len: int,
+    temperature: float = 0.6,
+    top_p: float = 0.9,
+) -> List[str]:
+    """
+    Args:
+        prompt_tokens (List[List[int]]): List of tokenized prompts, where each prompt is represented as a list of integers.
+        max_gen_len (int): Maximum length of the generated text sequence.
+        temperature (float, optional): Temperature value for controlling randomness in sampling. Defaults to 0.6.
+        top_p (float, optional): Top-p probability threshold for nucleus sampling. Defaults to 0.9.
+    """
+    params = generator.model.params
+    prompt_len = len(prompt_tokens)
+    total_len = min(params.max_seq_len, max_gen_len + prompt_len)
+
+    pad_id = generator.tokenizer.pad_id
+    tokens = torch.full((1, total_len,), pad_id, dtype=torch.long, device="cuda")
+    tokens[:, : len(prompt_tokens)] = torch.tensor(prompt_tokens, dtype=torch.long, device="cuda")
+
+    prev_pos = 0
+    eos_reached = torch.tensor([False], device="cuda")
+    input_text_mask = tokens != pad_id
+    if prompt_len == total_len:
+        logits = generator.model.forward(tokens, prev_pos)
+
+    stop_tokens = torch.tensor(list(generator.tokenizer.stop_tokens))
+    reply = []
+    for cur_pos in range(prompt_len, total_len):
+        logits = generator.model.forward(tokens[:, prev_pos:cur_pos], prev_pos)
+        if temperature > 0:
+            probs = torch.softmax(logits[:, -1] / temperature, dim=-1)
+            next_token = sample_top_p(probs, top_p)
+        else:
+            next_token = torch.argmax(logits[:, -1], dim=-1)
+
+        next_token = next_token.reshape(-1)
+        # only replace token if prompt has already been generated
+        next_token = torch.where(
+            input_text_mask[:, cur_pos], tokens[:, cur_pos], next_token
+        )
+        tokens[:, cur_pos] = next_token
+        eos_reached |= (~input_text_mask[:, cur_pos]) & (
+            torch.isin(next_token, stop_tokens)
+        )
+        prev_pos = cur_pos
+        if all(eos_reached):
+            break
+        word = generator.tokenizer.decode(next_token.tolist())
+        print(word, end="", flush=True)
+        reply.append(word)
+    return reply
+
+
 def main(
     ckpt_dir: str = "Meta-Llama-3-8B-Instruct",
     tokenizer_path: str = "Meta-Llama-3-8B-Instruct/tokenizer.model",
     temperature: float = 0.6,
     top_p: float = 0.9,
-    max_seq_len: int = 8000,
-    max_batch_size: int = 1,
+    max_seq_len: int = 6000,
     max_gen_len: Optional[int] = None,
 ):
     """
@@ -84,23 +141,23 @@ def main(
         ckpt_dir=ckpt_dir,
         tokenizer_path=tokenizer_path,
         max_seq_len=max_seq_len,
-        max_batch_size=max_batch_size,
+        max_batch_size=1,
     )
     print("context length:", max_seq_len)
     print(generator.model)
 
-    phrase_quit, phrase_new, phrase_text, phrase_pdf, phrase_url = "#QUIT", "#NEW", "#TEXT", "#PDF", "#URL"
+    phrase_quit, phrase_new, phrase_back, phrase_text, phrase_pdf, phrase_url = "#QUIT", "#NEW", "#BACK", "#TEXT", "#PDF", "#URL"
     preset = [
         {"role": "system", "content": "Think in steps before answering. Give straightforward and concise answers. If you do not know the answer, reply with I do not know."}
     ]
     print("stop inference server:  %s" % phrase_quit)
     print("start new conversation: %s" % phrase_new)
+    print("roll back one query:    %s" % phrase_back)
     print("read from text file:    %s <text-file-path>" % phrase_text)
     print("read from PDF file:     %s <PDF-file-path>" % phrase_pdf)
     print("read from webpage:      %s <URL>" % phrase_url)
 
     while True:
-        torch.cuda.empty_cache()
         print("============== start new 🦙 conversation ==============")
         context = copy.deepcopy(preset)
         context[0]["content"] += ' Now is %s.' % time.strftime('%H:%M %Z, %A, %Y %b %d')
@@ -113,6 +170,12 @@ def main(
                     return
                 elif query.lower().startswith(phrase_new.lower()):
                     break
+                elif query.lower().startswith(phrase_back.lower()):
+                    if len(context) > 1:
+                        context.pop(-1)
+                        context.pop(-1)
+                    print("🛑 > rolling back")
+                    continue
 
                 elif query.lower().startswith(phrase_text.lower()):
                     filename = query[len(phrase_text):].strip()
@@ -135,26 +198,27 @@ def main(
                         continue
                     query = "Read the following content fetched from the URL %s, and answer my questions about it. Give reference in the original content if possible. Start of the content:\n%s" % (url, text)
 
-            # print(query)
             context.append({"role": "user", "content": query})
+            torch.cuda.empty_cache()
 
-            try:
-                reply = generator.chat_completion(
-                    [context],
-                    max_gen_len=max_gen_len,
-                    temperature=temperature,
-                    top_p=top_p,
-                )[0]
-            except Exception as e:
-                print("🛑 > exception: %s, rolling back" % str(e))
+            prompt_tokens = generator.formatter.encode_dialog_prompt(context)
+            if len(prompt_tokens) >= generator.model.params.max_seq_len:
+                print("🛑 > maximum context length reached, rolling back")
                 context.pop(-1)
                 continue
 
-            tokens_count, time_gen = reply["tokens_count"], reply["time_gen"]
-            reply = reply["generation"]
-            assert reply["role"] == "assistant"
-            print("🦙 > %s [%d/%.2f = %.2f tokens/s]" % (reply["content"], tokens_count, time_gen, tokens_count / time_gen))
-            context.append(reply)
+            print("🦙 > ", end="", flush=True)
+            time0 = time.perf_counter()
+            reply = generate_print(
+                generator,
+                prompt_tokens=prompt_tokens,
+                max_gen_len=generator.model.params.max_seq_len - 1,
+                temperature=temperature,
+                top_p=top_p,
+            )
+            time_gen = time.perf_counter() - time0
+            print(" [%d/%.2f = %.2f tokens/s]" % (len(reply), time_gen, len(reply) / time_gen))
+            context.append({"role": "assistant", "content": "".join(reply)})
 
 
 if __name__ == "__main__":
